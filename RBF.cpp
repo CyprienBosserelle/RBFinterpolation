4b88b3b552cd3152c96882112266dd41fa87f3f0
Cyprien Bosserelle <cyprien.bosserelle@gmail.com>
2017-03-31 12:00:23 +1200

    Added the trining and interpolation as functions

--- RBF.cpp
+++ RBF.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <armadillo>
 
-#define pi 3.14159265
+//#define pi 3.14159265
 
 using namespace std;
 using namespace arma;
@@ -42,7 +42,133 @@ std::string trim(const std::string& str, const std::string& whitespace)
 	return str.substr(strBegin, strRange);
 }
 
+mat RBFgaussian(mat r, double gamma)
+{
+	//exp(-0.5 * r % r / (gamma * gamma));
+	return exp(-0.5 * r % r / (gamma * gamma));
+}
 
+double calculateGamma(int ncenters, int dims, mat r)
+{
+	double variance = -1;
+	double gamma;
+	for (int i = 0; i < ncenters; ++i)
+	{
+		for (int j = i + 1; j < ncenters; ++j)
+		{
+			variance = max(variance, (double)r(i,0) * (double)r(j,0)); // Need to check
+		}
+	}
+		
+	variance *= (1.0 / ((double)ncenters));
+	gamma = (-1.0 / (2.0 * variance));
+
+	return gamma;
+}
+
+/*double optimgamma(int ncenters, mat A0, mat RBFcoeff)
+{
+	//From:
+	// Alex Chirokov, alex.chirokov@gmail.com
+	// 16 Feb 2006
+	mat A, invA;
+
+	A = A0(span(0, ncenters - 1), span(0, ncenters - 1));
+	invA = pinv(A);
+
+
+}*/
+
+mat RBFtrain(int ncenters, int dim, double gamma, mat centersnorm, mat data)
+{
+	// Calculate the RBFcoeff 
+	mat A(ncenters, ncenters);
+	mat r, t, o, P, X, B, b, RBFcoeff;
+	mat test, rr, s, f;
+
+	A.zeros();
+
+	for (int i = 0; i < ncenters; i++)
+	{
+		//
+		for (int j = 0; j <= i; j++)
+		{
+			//
+			r = norm(centersnorm.col(i) - centersnorm.col(j));
+
+			t = exp(-0.5 * r % r / (gamma * gamma));// is 1 by 1 yeah?
+
+			A(i, j) = t(0, 0);
+			A(j, i) = t(0, 0);
+		}
+	}
+
+	o = ones(ncenters, 1);
+
+	P = join_rows(o, centersnorm.t());
+
+	X = join_rows(A, P);
+
+	B = join_rows(P.t(), zeros(dim + 1, dim + 1));
+	A = join_cols(X, B);
+
+	//A.save("A_mat.txt", raw_ascii);
+
+	b = join_cols(data.t(), zeros(dim + 1, 1));
+	//b.save("b_mat.txt", raw_ascii);
+
+	RBFcoeff = solve(A, b);
+
+	return RBFcoeff;
+
+}
+
+
+double RBFinterp(int ncenters, int dim, double gamma, mat RBFcoeff, mat centersnorm, mat pointsnorm)
+{
+	// perform the RBF interpolation using gamma, RBFcoeff the centers normalised and the new locations to interpolate to;
+	
+
+	mat rr, s, x;
+
+	x = centersnorm;
+
+	rr = zeros(1, ncenters);
+
+	rr = (pointsnorm*ones(1, ncenters)) - x;
+
+	rr = sqrt(sum(rr%rr, 0));
+
+	for (int j = 0; j < ncenters; j++)
+	{
+		rr(j) = norm(pointsnorm - x.col(j));
+	}
+
+	
+
+
+	mat tmp1, tmp2, tmp3;
+
+	tmp1 = exp(-0.5 * rr % rr / (gamma * gamma));
+	
+
+	tmp2 = RBFcoeff.rows(0, ncenters - 1);
+	
+
+	tmp3 = tmp2 % tmp1.t();
+	
+
+	s = RBFcoeff(ncenters) + sum(tmp3, 0);
+	
+
+	for (int k = 0; k < dim; k++)
+	{
+		s = s + RBFcoeff(ncenters + k + 1)*pointsnorm(k, 0);
+	}
+
+
+	return (double)s(0, 0);
+}
 
 
 int
@@ -53,7 +179,7 @@ main(int argc, char** argv)
 
 	mat x, y;
 
-	double cst = 0.2672;
+	double gamma = 0.2672;
 
 	int ncenters = 300;
 
@@ -63,10 +189,13 @@ main(int argc, char** argv)
 
 	vector<double> results;
 
-
+	// Set up variables
 	x = zeros(dim, ncenters);
 	y = zeros(1, ncenters);
 
+
+
+	//Open file containing RBF centers
 	std::ifstream fs;
 
 
@@ -100,7 +229,7 @@ main(int argc, char** argv)
 	fs.close();
 
 
-	// Normalise the centers to min max
+	// Calculate min and max for normalisation of centers and training data
 	double maxHs = x(0, 1);
 	double maxT = x(1, 1);
 	double maxNM = x(3, 1);
@@ -119,17 +248,18 @@ main(int argc, char** argv)
 		minT = min(x(1, n), minT);
 		minNM = min(x(3, n), minNM);
 	}
+	// Normalise the centers to min max
 
 	for (int n = 0; n < ncenters; n++)
 	{
 		x(0, n) = (x(0, n) - minHs) / (maxHs - minHs);
 		x(1, n) = (x(1, n) - minT) / (maxT - minT);
-		x(2, n) = x(2, n)*pi/180.0;
+		x(2, n) = x(2, n)*datum::pi/180.0;
 		x(3, n) = (x(3, n) - minNM) / (maxNM - minNM);
 	}
 
 
-
+	// Read the training data
 	fs.open("MauiBay_Shore_MDA_zs.txt");
 
 	if (fs.fail()){
@@ -155,8 +285,11 @@ main(int argc, char** argv)
 
 	
 	//train the RBF
-
-	mat A(ncenters, ncenters);
+	mat RBFcoeff;
+	RBFcoeff = RBFtrain(ncenters, dim, gamma, x, y);
+	mat test, rr;
+		/*
+		mat A(ncenters, ncenters);
 	mat r,t,o,P, X,B,b, RBFcoeff;
 	mat test, rr,s,f;
 
@@ -170,7 +303,7 @@ main(int argc, char** argv)
 			//
 			r = norm(x.col(i)-x.col(j));
 
-			t= exp(-0.5 * r % r / (cst * cst));// is 1 by 1 yeah?
+			t= exp(-0.5 * r % r / (gamma * gamma));// is 1 by 1 yeah?
 			
 			A(i, j) = t(0,0);
 			A(j, i) = t(0,0);
@@ -186,14 +319,14 @@ main(int argc, char** argv)
 	B = join_rows(P.t(), zeros(dim + 1, dim + 1));
 	A = join_cols(X, B);
 
-	A.save("A_mat.txt", raw_ascii);
+	//A.save("A_mat.txt", raw_ascii);
 
 	b = join_cols(y.t(), zeros(dim + 1, 1));
-	b.save("b_mat.txt", raw_ascii);
+	//b.save("b_mat.txt", raw_ascii);
 
 	RBFcoeff = solve(A, b);
-	RBFcoeff.save("RBFcoeff_mat.txt", raw_ascii);
-
+	//RBFcoeff.save("RBFcoeff_mat.txt", raw_ascii);
+	*/
 	mat readline = zeros(dim, 1);
 	test = zeros(dim, 49211);/////// !!!!!
 
@@ -235,48 +368,17 @@ main(int argc, char** argv)
 	{
 		test(0, n) = (test(0, n) - minHs) / (maxHs - minHs);
 		test(1, n) = (test(1, n) - minT) / (maxT - minT);
-		test(2, n) = test(2, n)*pi / 180.0;
+		test(2, n) = test(2, n)*datum::pi / 180.0;
 		test(3, n) = (test(3, n) - minNM) / (maxNM - minNM);
 	}
 
+	// Do the interpolation
 	rr = zeros(1, ncenters);
 
 	for (int n = 0; n < nline; n++)
 	{
-		rr = (test.col(n)*ones(1, ncenters)) - x;
-
-		rr = sqrt(sum(rr%rr, 0));
-
-		for (int j = 0; j < ncenters; j++)
-		{
-			rr(j) = norm(test.col(n) - x.col(j));
-		}
-
-		rr.save("rr_mat.txt", raw_ascii);
-
-
-		mat tmp1, tmp2, tmp3;
-
-		tmp1 = exp(-0.5 * rr % rr / (cst * cst));
-		tmp1.save("tmp1_mat.txt", raw_ascii);
-
-		tmp2 = RBFcoeff.rows(0, ncenters - 1);
-		tmp2.save("tmp2_mat.txt", raw_ascii);
-
-		tmp3 = tmp2 % tmp1.t();
-		tmp3.save("tmp3_mat.txt", raw_ascii);
-
-		s = RBFcoeff(ncenters) + sum(tmp3 , 0);
-		s.save("s_mat.txt", raw_ascii);
+		results.push_back(RBFinterp(ncenters, dim, gamma, RBFcoeff, x, test.col(n)));
 		
-		for (int k = 0; k < dim; k++)
-		{
-			s = s + RBFcoeff(ncenters + k + 1)*test(k, n);
-		}
-
-
-		results.push_back( s(0, 0));
-
 	}
 
 
