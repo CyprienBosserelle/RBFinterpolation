7d5c0604e87446aee952b79f57fd3fc8a97d9b62
Cyprien Bosserelle <cyprien.bosserelle@gmail.com>
2022-09-06 22:10:42 +1200

    Fix folllowing fopen change

--- read_input.cpp
+++ read_input.cpp
@@ -191,7 +191,7 @@ std::string trim(const std::string& str, const std::string& whitespace)
 void writedatafile(std::vector<double> outputdata, std::string outputfile)
 {
 	FILE * fs;
-	fopen(&fs,outputfile.c_str(),"w");
+	fs=fopen(outputfile.c_str(),"w");
 	for (int i = 0; i < outputdata.size(); i++)
 	{
 		//
